37a1daeb27bb53ddf2afc027b6a9a0688f8ceb1c
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-11-06 01:08:52 +0300

    create pitch model

--- app/models.py
+++ app/models.py
@@ -55,3 +55,24 @@ class Role(db.Model):
     def __repr__(self):
         return f'User {self.name}' 
 
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment',backref='pitch',lazy='dynamic')
+
+    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='pitch',lazy='dynamic')
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    category = db.Column(db.String(255), index = True,nullable = False)
+  
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Pitch {self.post}'

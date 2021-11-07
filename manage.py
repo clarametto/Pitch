a4f9cea7ac93be6404305dda8724b834dd2531a6
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-11-07 16:59:35 +0300

    update register.html

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User,Role
+from app.models import User
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
@@ -13,6 +13,6 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Role = Role )
+    return dict(app = app,db = db,User = User )
 if __name__ == '__main__':
     manager.run()

ff74e3e0f7453bafde7ca38d04425dc05a0153b4
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-11-07 15:47:53 +0300

    update email

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,4 @@
-# from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, SubmitField
 from wtforms.validators import Required
 

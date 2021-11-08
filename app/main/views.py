6a898815df4b0fc18e5569f42eae03de4fd843c5
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-11-08 12:11:54 +0300

    create landing, comment, pitch and profile in views

--- app/main/views.py
+++ app/main/views.py
@@ -8,10 +8,10 @@ from .. import db,photos
 @main.route('/')
 def index():
     pitches = Pitch.query.all()
-    technology = Pitch.query.filter_by(category = 'Technology').all() 
-    business = Pitch.query.filter_by(category = 'Business').all()
+    technology = Pitch.query.filter_by(category = 'Fashion').all() 
+    business = Pitch.query.filter_by(category = 'Science').all()
     programming = Pitch.query.filter_by(category = 'Programming').all()
-    religion = Pitch.query.filter_by(category = 'Religion').all()
+    religion = Pitch.query.filter_by(category = 'Education').all()
     sports = Pitch.query.filter_by(category = 'Sports').all()
     social = Pitch.query.filter_by(category = 'Social').all()
     return render_template('index.html', pitches = pitches, technology = technology,business = business,programming= programming,religion = religion,sports = sports,social = social)
@@ -55,59 +55,3 @@ def profile(name):
         abort(404)
 
     return render_template("profile/profile.html", user = user,posts=posts)
-
-@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
-@login_required
-def updateprofile(name):
-    form = UpdateProfile()
-    user = User.query.filter_by(username = name).first()
-    if user == None:
-        abort(404)
-    if form.validate_on_submit():
-        user.bio = form.bio.data
-        user.save_u()
-        return redirect(url_for('.profile',name = name))
-    return render_template('profile/update.html',form =form)
-
-@main.route('/user/<name>/update/pic',methods= ['POST'])
-@login_required
-def update_pic(name):
-    user = User.query.filter_by(username = name).first()
-    if 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        path = f'photos/{filename}'
-        user.profile_pic_path = path
-        db.session.commit()
-    return redirect(url_for('main.profile',name=name))
-
-@main.route('/like/<int:id>',methods = ['POST','GET'])
-@login_required
-def like(id):
-    get_pitches = Upvote.get_upvotes(id)
-    valid_string = f'{current_user.id}:{id}'
-    for pitch in get_pitches:
-        to_str = f'{pitch}'
-        print(valid_string+" "+to_str)
-        if valid_string == to_str:
-            return redirect(url_for('main.index',id=id))
-        else:
-            continue
-    new_vote = Upvote(user = current_user, pitch_id=id)
-    new_vote.save()
-    return redirect(url_for('main.index',id=id))
-
-@main.route('/dislike/<int:id>',methods = ['POST','GET'])
-@login_required
-def dislike(id):
-    pitch = Downvote.get_downvotes(id)
-    valid_string = f'{current_user.id}:{id}'
-    for p in pitch:
-        to_str = f'{p}'
-        print(valid_string+" "+to_str)
-        if valid_string == to_str:
-            return redirect(url_for('main.index',id=id))
-        else:
-            continue
-    new_downvote = Downvote(user = current_user, pitch_id=id)
-    new_downvote.save()
-    return redirect(url_for('main.index',id = id))

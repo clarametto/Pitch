c5f5e55659e13788ef9dc44470d302a0071da2b3
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-11-08 12:13:04 +0300

    create profile update in views

--- app/main/views.py
+++ app/main/views.py
@@ -55,3 +55,27 @@ def profile(name):
         abort(404)
 
     return render_template("profile/profile.html", user = user,posts=posts)
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save_u()
+        return redirect(url_for('.profile',name = name))
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<name>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',name=name))
